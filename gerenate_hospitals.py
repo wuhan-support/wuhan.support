3d0e1cbfa7d86f1bb6358c235c93dab881e6ef4c
Zhiyuan Chen <zc@int.ac.cn>
2020-01-25 04:09:08 +0800

    add file of gerenate hospitals

---
+++ gerenate_hospitals.py
@@ -0,0 +1,72 @@
+# -*- coding: UTF-8 -*-
+import os
+import time
+import csv
+
+
+def read(filename='./hospitals.csv'):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        hospitals = [hospital for hospital in reader]
+    return hospitals
+
+def write_md(hospital):
+    path = 'donate/{}.md'.format(hospital[0])
+    md = gerenate_md(hospital)
+    if os.path.exists(path):
+        return
+    with open(path, 'w+', encoding='utf-8') as f:
+        f.write(md)
+
+def gerenate_md(hospital):
+    md = ''
+    md += gerenate_header(hospital)
+    md += '\n'
+    md += gerenate_intro(hospital)
+    md += '\n'
+    md += gerenate_table(hospital)
+    md += '\n'
+    md += gerenate_contacts(hospital)
+    md += '\n'
+    return md        
+
+def gerenate_header(hospital):
+    header = '---\n'
+    header += 'title: {}\n'.format(hospital[2])
+    header += 'summary: {}\n'.format(hospital[3])
+    header += 'authors: \n'
+    for author in hospital[1].split('|'):
+        header += '    - {}\n'.format(author)
+    header += 'date: {}\n'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    header += 'categories: \n    - 捐赠\n'
+    header += 'tags: \n    - 捐赠\n'
+    header += '---\n'
+    return header
+
+def gerenate_intro(hospital):
+    intro = ''
+    for s in hospital[4].split('\\'):
+        intro += s
+        intro += '\n\n'
+    return intro[:-1]
+
+def gerenate_table(hospital):
+    table = '|  防护物资（耗材）  |  标准/要求  |\n|------------------|-----------|\n'
+    for supply, standard in zip(hospital[9].split('|'), hospital[10].split('|')):
+        table += '|  {}  |  {}\n'.format(supply, standard)
+    return table
+
+def gerenate_contacts(hospital):
+    contacts = ''
+    contacts += '地址：{}\n\n'.format(hospital[5])
+    if hospital[6]:
+        contacts += '电话：{}\n\n'.format(hospital[6])
+    contacts += '联系人：\n\n'
+    for contact, mobile in zip(hospital[7].split('|'), hospital[8].split('|')):
+        contacts += '+ {} {}\n'.format(contact, mobile)
+    return contacts[:-1]
+
+if __name__ == '__main__':
+    hospitals = read()
+    for hospital in hospitals[1:]:
+        write_md(hospital)
